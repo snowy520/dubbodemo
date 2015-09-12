ae3fe4625cc8a363d25d61b77a3be4d70415811e
Richard Xue <liming.snowy@gmail.com>
2015-09-12 15:54:11 +0800

    add dubbo demo

--- module1/src/main/java/cn/xxx/module1/Provider.java
+++ module1/src/main/java/cn/xxx/module1/Provider.java
@@ -10,9 +10,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class Provider {
     public static void main(String[] args) throws Exception {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"http://10.20.160.198/wiki/display/dubbo/provider.xml"});
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath*:spring-provider.xml");
         context.start();
-
         System.in.read(); // 按任意键退出
     }
 }
--- module2/src/main/java/cn/xxx/module2/Consumer.java
+++ module2/src/main/java/cn/xxx/module2/Consumer.java
@@ -1,5 +1,8 @@
 package cn.xxx.module2;
 
+import cn.xxx.module1.service.DemoService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * @author Richard Xue
  * @version 1.0
@@ -7,4 +10,11 @@ package cn.xxx.module2;
  * @description
  */
 public class Consumer {
+    public static void main(String[] args) throws Exception {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath*:spring-consumer.xml");
+        context.start();
+        DemoService demoService = (DemoService)context.getBean("demoService"); // 获取远程服务代理
+        String hello = demoService.sayHello("world"); // 执行远程方法
+        System.out.println( hello ); // 显示调用结果
+    }
 }
